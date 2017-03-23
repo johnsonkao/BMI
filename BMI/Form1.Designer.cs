7b12ce2c417ac7eca98ed430dd3732e386cf223f
johnsongau <johnsongau@DESKTOP-OB5EJ91>
2017-03-23 22:01:34 +0800

    加入專案檔案。

---
+++ BMI/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace BMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblBMI_Hint = new System.Windows.Forms.Label();
+            this.lblBMI = new System.Windows.Forms.Label();
+            this.lblSuggestion_hint = new System.Windows.Forms.Label();
+            this.lblSuggestion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(115, 30);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 22);
+            this.txtHeight.TabIndex = 0;
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(115, 72);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(100, 22);
+            this.txtWeight.TabIndex = 1;
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Location = new System.Drawing.Point(48, 33);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(29, 12);
+            this.lblWeight.TabIndex = 2;
+            this.lblWeight.Text = "身高";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "體重";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 52);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "計算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblBMI_Hint
+            // 
+            this.lblBMI_Hint.AutoSize = true;
+            this.lblBMI_Hint.Location = new System.Drawing.Point(50, 120);
+            this.lblBMI_Hint.Name = "lblBMI_Hint";
+            this.lblBMI_Hint.Size = new System.Drawing.Size(27, 12);
+            this.lblBMI_Hint.TabIndex = 7;
+            this.lblBMI_Hint.Text = "BMI";
+            // 
+            // lblBMI
+            // 
+            this.lblBMI.AutoSize = true;
+            this.lblBMI.Location = new System.Drawing.Point(117, 120);
+            this.lblBMI.Name = "lblBMI";
+            this.lblBMI.Size = new System.Drawing.Size(11, 12);
+            this.lblBMI.TabIndex = 8;
+            this.lblBMI.Text = "0";
+            // 
+            // lblSuggestion_hint
+            // 
+            this.lblSuggestion_hint.AutoSize = true;
+            this.lblSuggestion_hint.Location = new System.Drawing.Point(45, 159);
+            this.lblSuggestion_hint.Name = "lblSuggestion_hint";
+            this.lblSuggestion_hint.Size = new System.Drawing.Size(32, 12);
+            this.lblSuggestion_hint.TabIndex = 9;
+            this.lblSuggestion_hint.Text = " 建議";
+            // 
+            // lblSuggestion
+            // 
+            this.lblSuggestion.AutoSize = true;
+            this.lblSuggestion.Location = new System.Drawing.Point(117, 159);
+            this.lblSuggestion.Name = "lblSuggestion";
+            this.lblSuggestion.Size = new System.Drawing.Size(13, 12);
+            this.lblSuggestion.TabIndex = 10;
+            this.lblSuggestion.Text = "--";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(249, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "CM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(249, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "KG";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 203);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSuggestion);
+            this.Controls.Add(this.lblSuggestion_hint);
+            this.Controls.Add(this.lblBMI);
+            this.Controls.Add(this.lblBMI_Hint);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.txtHeight);
+            this.Name = "Form1";
+            this.Text = "BMI計算";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblBMI_Hint;
+        private System.Windows.Forms.Label lblBMI;
+        private System.Windows.Forms.Label lblSuggestion_hint;
+        private System.Windows.Forms.Label lblSuggestion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BMI/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI
+{
+    public partial class Form1 : Form
+    {
+        enum LEVEL {過輕,正常,過重,輕度肥胖,中度肥胖,重度肥胖};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal bmi=0;
+            decimal weight=0;
+            decimal height=0;
+            decimal.TryParse(txtWeight.Text, out weight);
+            decimal.TryParse(txtHeight.Text, out height);
+
+            if (height == 0)
+            {
+                MessageBox.Show("輸入錯誤");
+                return;
+            }
+            height = height / 100;
+            bmi = weight / (height * height);
+            lblBMI.Text = bmi.ToString("#.#");
+            if (bmi < 18.5m)
+                lblSuggestion.Text = LEVEL.過重.ToString();
+            else if (bmi >= 18.5m && bmi < 24m)
+                lblSuggestion.Text = LEVEL.正常.ToString();
+            else if (bmi >=24m && bmi <27m)
+                lblSuggestion.Text = LEVEL.過重.ToString();
+            else if (bmi >=27m && bmi < 30m)
+                lblSuggestion.Text = LEVEL.輕度肥胖.ToString();
+            else if (bmi >= 30m && bmi < 35m)
+                lblSuggestion.Text = LEVEL.中度肥胖.ToString();
+            else if (bmi >= 35m )
+                lblSuggestion.Text = LEVEL.重度肥胖.ToString();
+        }
+    }
+}
